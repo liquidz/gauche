f4467c15f044bb1669a121f9a439599b8294af51
liquidz <liquidz.uo@gmail.com>
2008-05-11 07:20:36 +0900

    twitter.scm supports SXML

--- module/twitter.scm
+++ module/twitter.scm
@@ -1,11 +1,22 @@
+;;; Twitter Module for Gauche
+;;; @author liquidz
+
 (define-module twitter
   (use rfc.uri)
   (use gauche.charconv)
+  (use sxml.ssax)
   (use mi.http)
-  (export-all)
+  (export
+    with-twitter twitter-update
+    twitter-friend-timeline twitter-user-timeline
+    )
   )
 (select-module twitter)
 
+;; =twitter
+;; Twitterクラス
+;; ユーザー名、パスワード、リクエスト先のURIなどを扱う。
+;; ---------------------------------------------------------
 (define-class <twitter> ()
   ((user :init-keyword :user)
    (password :init-keyword :password)
@@ -18,6 +29,11 @@
    (user/pass)
    )
   )
+
+;; =initialize
+;; Twitterクラスの初期化メソッド
+;; 取得したいフォーマットからリクエスト先URIなどを生成する
+;; ---------------------------------------------------------
 (define-method initialize ((self <twitter>) initargs)
   (next-method)
   (let ((user (ref self 'user)) (password (ref self 'password))
@@ -30,10 +46,22 @@
     )
   )
 
+;; =my-conv
+;; @strをURIエンコード＆UTF-8変換して返す
+;; ---------------------------------------------------------
 (define (my-conv str)
   (ces-convert (uri-encode-string str) "*JP")
   )
+;; =to-sxml
+;; @strをSXMLに変換して返す
+;; ---------------------------------------------------------
+(define (to-sxml str)
+  (ssax:xml->sxml (open-input-string str) '())
+  )
 
+;; =with-twitter
+;; @argsからTwitterオブジェクトを生成し、@fnに処理を渡す
+;; ---------------------------------------------------------
 (define (with-twitter fn . args)
   (let-keywords args ((user "") (password ""))
     (unless (or (string=? user "") (string=? password ""))
@@ -42,6 +70,10 @@
     )
   )
 
+;; =twitter-update
+;; @twitter-objに格納されている情報から
+;; @statusをステータスとして更新する
+;; ---------------------------------------------------------
 (define (twitter-update twitter-obj status)
   (if (< (string-length status) (ref twitter-obj 'status-max))
     (let1 uri #`",(ref twitter-obj 'update)?status=,(my-conv status)"
@@ -50,14 +82,26 @@
     )
   )
 
+;; =twitter-friend-timeline
+;; @twitter-objに格納されている情報から
+;; フレンドのタイムラインを取得し、SXMLとして結果を返す
+;; ---------------------------------------------------------
 (define (twitter-friend-timeline twitter-obj)
-  (open-uri (ref twitter-obj 'friend-timeline)
-            :user/pass (ref twitter-obj 'user/pass))
+  (to-sxml
+    (open-uri (ref twitter-obj 'friend-timeline)
+              :user/pass (ref twitter-obj 'user/pass))
+    )
   )
 
+;; =twitter-user-timeline
+;; @twitter-objに格納されている情報から
+;; 自分のタイムラインを取得し、SXMLとしｔ結果を返す
+;; ---------------------------------------------------------
 (define (twitter-user-timeline twitter-obj)
-  (open-uri (ref twitter-obj 'user-timeline)
-            :user/pass (ref twitter-obj 'user/pass))
+  (to-sxml
+    (open-uri (ref twitter-obj 'user-timeline)
+              :user/pass (ref twitter-obj 'user/pass))
+    )
   )
 
 (provide "twitter")
